7669fad20a5a85f3b43f13fe40316e695bd97a46
dimok <zhasulanov04@list.ru>
2023-02-24 05:26:04 +0600

    Implement bussness-logic cart

--- internal/data/cart_item.go
+++ internal/data/cart_item.go
@@ -1,13 +1,14 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
 
 type CartItem struct {
 	ID           int64     `json:"id"`
-	SessionID    int64     `json:"user_id"`
+	SessionID    int64     `json:"session_id"`
 	ProductID    int64     `json:"product_id"`
 	Quantity     int64     `json:"quantity"`
 	CreationDate time.Time `json:"creation_date"`
@@ -15,4 +16,50 @@ type CartItem struct {
 
 type CartItemModel struct {
 	DB *sql.DB
+}
+
+func (i CartItemModel) Insert(sessionId int64, productID int64, quantity int64) error {
+	query := `
+	INSERT INTO card_items (sessionId, product_id, quantity)
+	VALUES ($1, $2, $3)`
+
+	args := []any{sessionId, productID, quantity}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := i.DB.QueryRowContext(ctx, query, args...)
+	if err != nil {
+		return err.Err()
+	}
+	return nil
+}
+
+func (i CartItemModel) Update(item *CartItem) error {
+	query := `UPDATE cart_items
+	SET quantity = $1
+	WHERE id = $2`
+
+	return i.DB.QueryRow(query, item.Quantity, item.ID).Err()
+}
+
+func (i CartItemModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM card_items WHERE id = $1`
+	result, err := i.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
 }
--- internal/data/shopping_session.go
+++ internal/data/shopping_session.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
@@ -8,10 +9,105 @@ import (
 type ShoppingSession struct {
 	ID           int64     `json:"id"`
 	UserID       int64     `json:"user_id"`
-	total        float64   `json:"total"`
+	Total        float64   `json:"total"`
 	CreationDate time.Time `json:"creation_date"`
 }
 
 type ShoppingSessionModel struct {
 	DB *sql.DB
+}
+
+func (s ShoppingSessionModel) Insert(userID int64) error {
+	query := `
+	INSERT INTO shopping_session (user_id, total) 
+	VALUES ($1, $2)
+	RETURNING version`
+
+	args := []any{userID, 0}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var version int64
+	err := s.DB.QueryRowContext(ctx, query, args...).Scan(&version)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s ShoppingSessionModel) Get(userID int64) (*ShoppingSession, error) {
+	query := `SELECT id, user_id, total, creation_date FROM carts WHERE user_id = $1`
+
+	var session ShoppingSession
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := s.DB.QueryRowContext(ctx, query, userID).Scan(
+		&session.ID,
+		&session.UserID,
+		&session.Total,
+		&session.CreationDate,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &session, nil
+}
+
+func (s ShoppingSessionModel) GetCartItems(id int64) ([]*CartItem, error) {
+	query := `
+	SELECT id, session_id, product_id, quantity, creation_date
+	FROM cart_items
+	WHERE session_id = $1`
+
+	var items []*CartItem
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := s.DB.QueryContext(ctx, query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var item CartItem
+		err := rows.Scan(
+			&item.ID,
+			&item.SessionID,
+			&item.ProductID,
+			&item.Quantity,
+			&item.CreationDate,
+		)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, &item)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func (s ShoppingSessionModel) GetCartTotal(id int64) (float64, error) {
+	query := `
+	SELECT SUM(price * quantity) 
+	FROM cart_items 
+	INNER JOIN products ON cart_items.product_id = products.id 
+	WHERE session_id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var total float64
+	err := s.DB.QueryRowContext(ctx, query, id).Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
 }
