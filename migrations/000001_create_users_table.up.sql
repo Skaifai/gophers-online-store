3016164cb0324f2bc607e588e30b4f027d8cd09f
Arman <real.alzhanov@gmail.com>
2023-02-23 21:57:27 +0600

    Add migrations

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,11 +2,18 @@ CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
     -- id column is a 64-bit auto-incrementing integer & primary key (defines the row)
     id bigserial PRIMARY KEY,
-    role varchar(10) not null,
-    name varchar(20) not null,
+    role varchar(10) not null default 'USER',
+    username varchar(20) UNIQUE not null,
     email citext UNIQUE not null,
+    phone_number varchar(15) not null,
     password_hash bytea not null,
     registration_date timestamp(0) with time zone not null default NOW(),
-    profile bigint UNIQUE not null,
+    name varchar(20) not null,
+    surname varchar(20) not null,
+    date_of_birth date not null,
+    address text not null,
+    about_me text not null,
+    picture_url text not null default 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    activated boolean not null default false,
     version integer NOT NULL DEFAULT 1
 );
---
+++ migrations/000002_create_products_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS products (
+    -- id column is a 64-bit auto-incrementing integer & primary key (defines the row)
+     id bigserial PRIMARY KEY,
+     name varchar(20) not null,
+     price float8 not null,
+     description text not null,
+     category varchar(20) not null,
+     quantity int not null default 1,
+     is_available boolean not null default true,
+     creation_date timestamp(0) with time zone not null default NOW(),
+     version integer NOT NULL DEFAULT 1
+);
